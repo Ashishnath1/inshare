3e3a5d5caefb6e72d972968a905628140ca856a0
Ashishnath1 <90963241+Ashishnath1@users.noreply.github.com>
2021-09-20 02:09:59 +0530

    Update download.js

--- routes/download.js
+++ routes/download.js
@@ -4,6 +4,7 @@ const File = require("../models/file");
 
 router.get("/:uuid", async (req, res) =>{
 
+    try{
     const file = await File.findOne({uuid: req.params.uuid});
 
     if(!file){
@@ -13,7 +14,10 @@ router.get("/:uuid", async (req, res) =>{
     const response = await file.save();
     const filePath = `${__dirname}/../${file.path}`;
     res.download(filePath);
-    
+    }
+    catch(err){
+        return res.render("download", {error: "Oops! something went wrong"});
+    }
 })
 
 module.exports = router;
